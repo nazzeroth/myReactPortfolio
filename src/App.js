d3e87d7d7b4d888f37be48322c8ed3c078314c2e
nazzeroth <nazzeroth@gmail.com>
2021-02-20 13:35:32 +0100

    personaldata

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './Header/Header.js';
 import Self from "./SelfPicture/Selfpicture.js";
 import Personaldata from "./Personaldata/Personaldata.js";
+import Personalbottom from "./Personalbottom/Personalbottom.js";
 
 function App() {
   return (
-    <div >
+    <div className="font">
     <Header />
     <Self />
     <Personaldata />
+    <Personalbottom />
     </div>
   );
 }
---
+++ src/Personalbottom/Personalbottom.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Personalbottom.css";
+
+const Personalbottom =()=> {
+    return (<div className="div-personal-bottom">
+    <div>
+    <p className="icon">logó/ikon</p>
+    <h4>Profil</h4>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+    </div>
+
+    <div>
+    <p className="icon">logó/ikon</p>
+    <h4>address</h4>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+    </div>
+
+    <div>
+    <p className="icon">logó/ikon</p>
+    <h4>Hobbik stb</h4>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+    </div>
+
+    </div>)
+}
+
+export default Personalbottom;
