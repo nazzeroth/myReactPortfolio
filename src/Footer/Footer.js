20ec04384dd29a85400ba35bd4371bca05d27b11
nazzeroth <nazzeroth@gmail.com>
2021-02-20 16:22:54 +0100

    szerkezet kész majdnem

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Footer.css";
-import logo from "./reactlogo.png";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const Footer =()=> {
     return (<div className="footer">
-        <footer >
-            <span>&copy; Dalocsa Gábor 2021</span>
+        <footer>
+            <div>
+            <a href="https://www.linkedin.com/in/g%C3%A1bor-dalocsa-76a397198/" target="_blank" className="footer-icon"><LinkedInIcon fontSize='large'/></a>
+            <a href="https://www.facebook.com/gabor.dalocsa.7/" target="_blank" className="footer-icon"><FacebookIcon fontSize='large'/></a>
+            <a href="https://github.com/nazzeroth/" target="_blank" className="footer-icon"><GitHubIcon fontSize='large'/></a>
+                <h2>© Copyright 2021 Dalocsa Gábor</h2>
+            </div>
         </footer>
-
     </div>)
 }
 
