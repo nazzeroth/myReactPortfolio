5fc6f305605ab8c1aa78466a821d88bfc5cd383c
nazzeroth <nazzeroth@gmail.com>
2021-02-20 19:06:22 +0100

    majdnem kész

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,6 +3,8 @@ import "./Footer.css";
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import DescriptionIcon from '@material-ui/icons/Description';
+import cv from "./dalocsa_gabor.pdf";
 
 const Footer =()=> {
     return (<div className="footer">
@@ -11,6 +13,7 @@ const Footer =()=> {
             <a href="https://www.linkedin.com/in/g%C3%A1bor-dalocsa-76a397198/" target="_blank" className="footer-icon"><LinkedInIcon fontSize='large'/></a>
             <a href="https://www.facebook.com/gabor.dalocsa.7/" target="_blank" className="footer-icon"><FacebookIcon fontSize='large'/></a>
             <a href="https://github.com/nazzeroth/" target="_blank" className="footer-icon"><GitHubIcon fontSize='large'/></a>
+            <a href={cv} className="footer-icon"><DescriptionIcon fontSize='large'/></a>
                 <h2>© Copyright 2021 Dalocsa Gábor</h2>
             </div>
         </footer>
--- src/Skill/Skill.js
+++ src/Skill/Skill.js
@@ -1,11 +1,47 @@
 import React from "react";
 import "./Skill.css";
-import skillpicture from "./SkillPicture.jpg";
+import skillpicture from "./SkillPicture2.jpg";
 
 const Skill =()=> {
- return (<div>
+ return (<div className="skill-div">
     <img src={skillpicture} className="skillpicture"></img>
-
+    <div>
+   <p className="skill-p">Tech skill</p>
+   </div>
+   <div>
+      <table className="table">
+         <tr>
+            <td>Soft skill:</td>
+            <td>Tervezési készség, Rugalmasság, Kommunikáció, Monotonítás tűrés, Precizítás</td>
+         </tr>
+         
+         
+         <tr>
+            <td>Hard skill</td>
+            <td></td>
+         </tr>
+         <tr>
+            <td className="td-inherit">Backend:</td>
+            <td>C#, JavaScriptm, NodeJs/ExpressJs</td>
+         </tr>
+         <tr>
+            <td className="td-inherit">Frontend:</td>
+            <td>HTML5/CSS3, ReactJS, BootStrap, jQuery, Ajax</td>
+         </tr>
+         <tr>
+            <td className="td-inherit">Verziókezelés:</td>
+            <td>Git, GitHub, SvN</td>
+         </tr>
+         <tr>
+            <td className="td-inherit">Adatbázis kezelés:</td>
+            <td>MySQL/MsSQL, NoSQL/MongoDB</td>
+         </tr>
+         <tr>
+            <td className="td-inherit">Rendszerismeret:</td>
+            <td>Windows, Linux/Unix/bash</td>
+         </tr>
+      </table>
+   </div>
  </div>)
 }
 
