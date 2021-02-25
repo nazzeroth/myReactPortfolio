a987f0e7a41f713e76e386fb32f6120de76a6cc9
nazzeroth <nazzeroth@gmail.com>
2021-02-25 14:26:41 +0100

    deploy ready

--- src/Personalbottom/Personalbottom.js
+++ src/Personalbottom/Personalbottom.js
@@ -12,7 +12,7 @@ class Personalbottom extends React.Component {
     <ScrollAnimation animateIn="fadeIn" animateOnce="true" duration="3">
     <span><AccountCircleIcon className="icon"/></span>
     <h4>Érdeklődés</h4>
-    <p>Programozás, érdekelt mindig is ,hogy mi miért mükődik , anno ez kezdődött a hardwarekkel, és folytatódott a programozással, nagyon megszerettem a webfejlesztést, és amikor csak időm engedi fejlesztem magam és tanulok, hogy jobb lehessek.</p>
+    <p>Programozás érdekelt mindig is, hogy mi miért működik. Anno ez kezdődött a hardverekkel és folytatódott a programozással. Nagyon megszerettem a webfejlesztést és amikor csak időm engedi fejlesztem magam és tanulok, hogy minél jobb lehessek ezen a területen.</p>
     </ScrollAnimation>
     </div>
 
@@ -20,7 +20,7 @@ class Personalbottom extends React.Component {
     <ScrollAnimation animateIn="fadeIn" animateOnce="true" duration="3">
     <span><ContactsIcon className="icon"/></span>
     <h4>Elérhetőség</h4>
-    <p>Magyarország, Budapest. Mobilis vagyok, nem okoz problémát az utazás, és a távolság, preferálom a kerékpáros közlekedést, ha tehetem mindenhova azzal megyek. De 2021-ben a leggyorsabb elérhetőség az <a href="mailto:dalocsagabor@outlook.com" className="email">email</a> !</p>
+    <p>Magyarország, Budapest. Teljesen mobilis vagyok, egyáltalán nem okoz problémát az utazás és a távolság. Preferálom a kerékpáros közlekedést, ha tehetem mindenhova azzal megyek. De 2021-ben a leggyorsabb elérhetőség az <a href="mailto:dalocsagabor@outlook.com" className="email">email</a> !</p>
     </ScrollAnimation>
     
     
@@ -30,7 +30,7 @@ class Personalbottom extends React.Component {
     <ScrollAnimation animateIn="fadeIn" animateOnce="true" duration="3">
     <span><DirectionsBikeIcon className="icon"/></span>
     <h4>Hobbi</h4>
-    <p>Aktív kikapcsolódást részesítem előnyben, szeretek kerékpározni, szeretem a természetetet és a víz közelségét ebből kifolyólag szeretek horgászni, ha adrenalin bombára vágyok, akkor airsoft és paintballozok a barátokkal, mind emelett makettezek is, ahol kiélhetem creativításomat.</p>
+    <p>Aktív kikapcsolódást részesítem előnyben, szeretek kerékpározni, szeretem a természetetet és a víz közelségét. Ebből kifolyólag szeretek horgászni, viszont ha adrenalin bombára vágyok, akkor airsoft és paintballozok a barátokkal, mindemellett makettezek is, ahol kiélhetem kreativitásomat.</p>
     </ScrollAnimation>
     </div>
 
--- src/Personaldata/Personaldata.js
+++ src/Personaldata/Personaldata.js
@@ -9,15 +9,7 @@ class Personaldata extends React.Component {
         <div>
         <h1> Egy kicsit magamról.</h1>
         <h3>szeretném bemutatni magam blababal</h3>
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.
-        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-        looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-         and going through the cites of the word in classical literature, discovered the undoubtable source.
-          Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
-          (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory
-          of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..
-          ", comes from a line in section 1.10.32.</p>
+        <p>Már kiskoromban látszódott az affinitásom a technológiák felé, amikor 2 évesen alig bírtam járni de szüleim nagy meglepetésére már a videó lejátszót szereltem hogy tudjam nézni a kedvenc meséimet. Ez nyilván tovább erősödött bennem egészen felnőtt koromig amikor is hardwarekkel kezdtem el komolyabban foglalkozni. 10 évig jártam ezt az utat, viszont egy idő után sajnos ebben már nem maradt sok kihívás a számomra, így úgy döntöttem, ideje valami újba kezdenem. Mivel mindig is ebben a szakmában éreztem otthon magamat és nagyon érdekelt hogy mi az ami ezeket a hardwareket 'meghajtja',  eldöntöttem hogy fejlesztő szeretnék lenni. Ez volt az a pillanat az életemben amikor kinyílt előttem a világ. Észveszejtő hogy pár száz vagy ezer karakter megírásával képesek vagyunk eszközöket irányítani, olyan eszközöket amiket én előtte sokáig bütyköltem csavarhúzóval. Azóta is szabadidőm nagy részét azzal töltöm hogy minél többet megtanuljak erről a témakörről.</p>
         </div>
         </ScrollAnimation>
     </div>
